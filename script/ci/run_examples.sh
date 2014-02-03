561e166b1181ee995093f8dbffb051496a18edff
Nicholas Kiraly <kiraly.nicholas@gmail.com>
2014-02-03 14:25:51 -0500

    use TRAVIS_DBSTEWARD_BINARY_LOCATION env variable for project.dbsteward.path when defined

--- script/ci/run_examples.sh
+++ script/ci/run_examples.sh
@@ -6,12 +6,28 @@
 
 basedir=`pwd`
 
+mvn_props=""
+
+# if travis specified the binary location via environment variable
+# then us it when calling the maven plugin
+if [ -z "${TRAVIS_DBSTEWARD_BINARY_LOCATION}" ]; then
+  mvn_props="" ;
+else
+  echo "Using environment variable TRAVIS_DBSTEWARD_BINARY_LOCATION ${TRAVIS_DBSTEWARD_BINARY_LOCATION} for project.dbsteward.path"
+  mvn_props="-D project.dbsteward.path=${TRAVIS_DBSTEWARD_BINARY_LOCATION}" ;
+fi
+
+
+dropdb -U dbsteward_ci someapp
+
+
 cd ${basedir}
 cd example1
-mvn clean
-mvn dbsteward-maven-plugin:sql-compile dbsteward-maven-plugin:db-create
+#mvn clean
+mvn dbsteward-maven-plugin:sql-compile dbsteward-maven-plugin:db-create ${mvn_props}
+
 
 cd ${basedir}
 cd example2
-mvn clean
-mvn dbsteward-maven-plugin:sql-diff dbsteward-maven-plugin:db-upgrade
+#mvn clean
+mvn dbsteward-maven-plugin:sql-diff dbsteward-maven-plugin:db-upgrade ${mvn_props}
